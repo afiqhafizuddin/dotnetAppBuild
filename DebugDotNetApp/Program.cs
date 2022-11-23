c64929d3dbce269c31edd87941468598fe1e66fb
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-11-23 23:30:28 +0800

    learned .NET debugger using VS code

--- DebugDotNetApp/Program.cs
+++ DebugDotNetApp/Program.cs
@@ -1,4 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Afiq Hafizuzddin bin Zainal");
+﻿int result = Fibonacci(5);
+Console.WriteLine(result);
 
+static int Fibonacci(int n)
+{
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i <= n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+
+    return n == 0 ? n1 : n2; 
+}
