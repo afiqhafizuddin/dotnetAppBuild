0455aa46854520e5d436de210f1f91772704caef
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-12-01 23:53:00 +0800

    add sln to the new exercise working with directories in .NET

---
+++ dirInDotNet/Program.cs
@@ -0,0 +1,20 @@
+﻿// Work with the file system
+
+// .NET Contains built-in type for working with file systwm thru System.IO namespace
+// 
+
+using System.IO;
+
+//IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("directories\\stores");
+IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("stores");
+
+foreach (var directory in listOfDirectories)
+{
+    Console.WriteLine(directory);
+}
+
+IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles("stores", "*.txt", SearchOption.AllDirectories);
+foreach (var file in allFilesInAllFolders)
+{
+    Console.WriteLine(file);
+}
