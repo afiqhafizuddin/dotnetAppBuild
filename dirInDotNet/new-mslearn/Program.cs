0ede4421cc16b9f78583979aabb2bf0fac99e3b3
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-12-03 22:09:54 +0800

    track mslearn directory

---
+++ dirInDotNet/new-mslearn/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+
+var salesFiles = FindFiles("stores");
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+IEnumerable<string> FindFiles(string folderNames)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderNames, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain full path, so just the check the end of it
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+    return salesFiles;
+}
+
+Console.WriteLine(Directory.GetCurrentDirectory());
+
+string documentPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+Console.WriteLine(documentPath);
