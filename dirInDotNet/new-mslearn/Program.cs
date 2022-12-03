d0a4417807baa7a67032e363a4dc5c536d1d2b84
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-12-04 01:19:00 +0800

    add code to work with directory information

--- dirInDotNet/new-mslearn/Program.cs
+++ dirInDotNet/new-mslearn/Program.cs
@@ -32,3 +32,11 @@ string documentPath = Environment.GetFolderPath(Environment.SpecialFolder.UserPr
 Console.WriteLine(documentPath);
 
 Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+Console.WriteLine(Path.Combine("stores", "201"));
+Console.WriteLine(Path.GetExtension("sales.json"));
+
+string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+
+FileInfo info = new FileInfo(fileName);
+
+Console.WriteLine($"Full name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Create date: {info.CreationTime}");
